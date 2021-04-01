5639417fa12db5bc9e035dc3d85d8fb94332c294
NukeNalin <nalinporwal10@gmail.com>
2021-04-01 13:11:29 +0530

    1 April Palindrome Linked List Solution

--- April 2021/1stPalindromeLinkedListSolution/PalindromeLinkedList Solution/PalindromeLinkedList Solution/main.swift
+++ April 2021/1stPalindromeLinkedListSolution/PalindromeLinkedList Solution/PalindromeLinkedList Solution/main.swift
@@ -1,37 +1,21 @@
 //
 //  main.swift
-//  1stApril2020
+//  PalindromeLinkedList Solution
 //
 //  Created by Nalin iOS Developer on 01/04/21.
 //
 
-/// ` Palindrome Linked List`
-/// `LeetCode 1st April 2021`
-
 import Foundation
 
 public class ListNode {
-
     public var val: Int
     public var next: ListNode?
-
-    public init() {
-        self.val = 0
-        self.next = nil
-    }
-    public init(_ val: Int) {
-        self.val = val
-        self.next = nil
-    }
-    public init(_ val: Int, _ next: ListNode?) {
-        self.val = val
-        self.next = next
-    }
-    
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
 }
-
+ 
 class Solution {
-    // MARK: - isPalindrome
     func isPalindrome(_ head: ListNode?) -> Bool {
         if head == nil { return false }
         var currentNode = head
